a3288e8ebfaa0f5fb313bc98ef543d7dc055b153
Himnyaga <alexis972005@gmail.com>
2025-01-08 10:36:36 -0400

    init

---
+++ src/main/java/stage/aratus/inventaire_back/InventaireBackApplication.java
@@ -0,0 +1,13 @@
+package stage.aratus.inventaire_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventaireBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventaireBackApplication.class, args);
+	}
+
+}
