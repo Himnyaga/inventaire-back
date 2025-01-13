39344e4d68a16130c5292e1a208edc41db1ad2c3
Himnyaga <alexis972005@gmail.com>
2025-01-13 12:45:47 -0400

    push

---
+++ inv-back/src/main/java/stage/aratus/inventaire_back/ServletInitializer.java
@@ -0,0 +1,13 @@
+package stage.aratus.inventaire_back;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(InventaireBackApplication.class);
+	}
+
+}
---
+++ inv-back/src/test/java/stage/aratus/inventaire_back/InventaireBackApplicationTests.java
@@ -0,0 +1,13 @@
+package stage.aratus.inventaire_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventaireBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
